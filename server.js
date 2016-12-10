0c2fee50884a4148132b0dfd1a4ef8a2a584b11e
Natalya Buchwald <natalyabuchwald@Natalyas-MacBook-Pro.local>
2016-12-10 18:58:46 -0500

    Added server,js and initialized express and get/post

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
 //Initialize Express
-
 const express = require('express');
+const bodyParser= require('body-parser'); //needed to parse post requests
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true})); //use bodyparser for forms
+
+//localhost server
+app.listen(3000, function() {
+  console.log('Listening on 3000...')
+});
+
+//GET Request
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+  //__dirname = /Users/natalyabuchwald/Desktop/portfoliobuilder2
+});
+
+//POST Request
+app.post('/projects', (req, res) => {
+  console.log(req.body)
+})
+
