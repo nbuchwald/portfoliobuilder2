7a22388809dbffe7e2ca4b6707abf1323ae69985
Natalya Buchwald <natalyabuchwald@Natalyas-MacBook-Pro.local>
2016-12-10 19:24:37 -0500

    Added ejs and views folder as rendering engine

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true})); //use bodyparser for forms
 
-
+//set ejs as view engine
+app.set('view engine', 'ejs')
 
 
 //mongoDB connection
@@ -17,7 +18,7 @@ MongoClient.connect('mongodb://nbuchwald:bubbi727@ds159517.mlab.com:59517/porfol
   		db = database
   	//localhost server
   		app.listen(3000, () => {
-    		console.log('Listening on 3000...db')
+    		console.log('Listening on 3000...with db connection')
   		})
   
 });
@@ -27,7 +28,15 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
   //__dirname = /Users/natalyabuchwald/Desktop/portfoliobuilder2
   var cursor = db.collection('projects').find()
+  //projects: db collection retrieval
+  db.collection('projects').find().toArray(function(err, results) {
+  console.log(results)
+  // send HTML file populated with quotes here
+})
 });
+
+
+
 
 //POST Request
 app.post('/projects', function (req, res) {
