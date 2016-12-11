031891d03006c664b10098ad56ed5927872462a3
Natalya Buchwald <natalyabuchwald@Natalyas-MacBook-Pro.local>
2016-12-10 19:17:53 -0500

    Data saved to MongoDB (portfoliobuilder)

--- server.js
+++ server.js
@@ -5,19 +5,37 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true})); //use bodyparser for forms
 
-//localhost server
-app.listen(3000, function() {
-  console.log('Listening on 3000...')
+
+
+
+//mongoDB connection
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://nbuchwald:bubbi727@ds159517.mlab.com:59517/porfoliobuilder'
+	, function (err, database) {
+		if (err) return console.log(err)
+  		db = database
+  	//localhost server
+  		app.listen(3000, () => {
+    		console.log('Listening on 3000...db')
+  		})
+  
 });
 
 //GET Request
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html')
   //__dirname = /Users/natalyabuchwald/Desktop/portfoliobuilder2
+  var cursor = db.collection('projects').find()
 });
 
 //POST Request
-app.post('/projects', (req, res) => {
-  console.log(req.body)
+app.post('/projects', function (req, res) {
+	db.collection('projects').save(req.body, function (err, result) {
+    if (err) return console.log(err)
+
+    console.log('Saved to Database')
+    res.redirect('/')
+  })
 })
 
