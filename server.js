1dd2f2c4aaf65040c63d80b779e5fb9cf054c1b6
Natalya Buchwald <natalyabuchwald@Natalyas-MacBook-Pro.local>
2016-12-10 21:56:24 -0500

    Correctly saves form submits to mongo DB

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 //Initialize Express
 const express = require('express');
 const bodyParser= require('body-parser'); //needed to parse post requests
+var morgan = require('morgan');
+app.use(express.static(__dirname + '/'))
 const app = express();
 
+app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({extended: true})); //use bodyparser for forms
 
 //set ejs as view engine
@@ -11,40 +14,51 @@ app.set('view engine', 'ejs')
 
 //mongoDB connection
 const MongoClient = require('mongodb').MongoClient
-
+//var projects = [];
 MongoClient.connect('mongodb://nbuchwald:bubbi727@ds159517.mlab.com:59517/porfoliobuilder'
 	, function (err, database) {
 		if (err) return console.log(err)
   		db = database
   	//localhost server
-  		app.listen(3000, () => {
+  		app.listen(3000, function() {
     		console.log('Listening on 3000...with db connection')
   		})
   
 });
+//GET Request Ver 2
+app.get('/', (req, res) => {
+  db.collection('projects').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {projects: result})
+  })
+})
 
 //GET Request
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html')
-  //__dirname = /Users/natalyabuchwald/Desktop/portfoliobuilder2
-  var cursor = db.collection('projects').find()
-  //projects: db collection retrieval
-  db.collection('projects').find().toArray(function(err, results) {
-  console.log(results)
-  // send HTML file populated with quotes here
-})
-});
+// app.get('/', function (req, res) {
+//   res.render(views, locals);
+//   res.sendFile(__dirname + '/views/index.ejs')
+//   //__dirname = /Users/natalyabuchwald/Desktop/portfoliobuilder2
+//   var cursor = db.collection('projects').find();
+//   //projects: db collection retrieval
+//   db.collection('projects').find().toArray(function(err, results) {
+//   	if (err) return console.log(err)
+//     // renders index.ejs
+//     res.render('index.ejs', {projects: results});
+  
+// 	})
+// });
 
 
 
 
 //POST Request
 app.post('/projects', function (req, res) {
-	db.collection('projects').save(req.body, function (err, result) {
+	db.collection('projects').save(req.body, function (err, results) {
     if (err) return console.log(err)
 
-    console.log('Saved to Database')
-    res.redirect('/')
+    console.log('Saved to Database');
+    res.redirect('/');
   })
 })
 
