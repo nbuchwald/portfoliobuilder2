613da8b4754b98acdff8326aed8bb20d0b55e03e
Natalya Buchwald <natalyabuchwald@Natalyas-MBP.wv.cc.cmu.edu>
2016-12-11 21:09:59 -0500

    Added the Google Drive API

--- javascript.js
+++ javascript.js
@@ -1,13 +1,64 @@
 //javascript.js
 
 $(document).ready(function(){
-	$('form').submit(function (event) {
-		console.log("got here but what?????! hi")
-	  //  event.preventDefault();
-	  // var newProjectAddedAlert = $('<div class="alert alert-success" role="alert">' +
-   // 'You successfully added a new project!' +'</div>');
-	  // $('.rowOfProjects').before(newProjectAddedAlert);
-	  
-	});
-})
+	
+//   var projectData;
+  
+//   // Load last project from local storage
+//   loadDataFromLocalStorage();
+
+//   // On tap of increment button
+//   $('#add').tap(function() {
+//     // Increment the count
+//     countData.count++;
+//     // Update the display
+//     $('#count').text(countData.count);
+//     // Save data to local storage
+//     saveDataToLocalStorage();
+//     // Clear the status line in case it has an old message
+//     $('#status').text("");
+//   });
+  
+//   // On tap of the refresh button
+//   $('#refresh').tap(function() {
+//     // Try to refresh data from the server
+//     $.getJSON('count.json')
+//       .done(function(data) {
+//         // Upon success, update status line
+//         $('#status').text("Successful refresh");
+//         // Replace countData with data returned from server
+//         countData = data;
+//         // Update the displayed count
+//         $('#count').text(countData.count);
+//         //Save the updated count
+//         saveDataToLocalStorage();
+//       }) // notice no semicolon for these methods are chained together
+//       .fail(function() {
+//         // Update status if Ajax failed (e.g. network down, server down)
+//         $('#status').text("Refresh failed");
+//       });
+//   });
+  
+//   function saveDataToLocalStorage() {
+//     // Turn countData into a JSON string, and store it to localStorage
+//     localStorage.countData = JSON.stringify(countData);
+//   }
+
+//   function loadDataFromLocalStorage() {
+//     // If countData has been stored to localStorage
+//     if (localStorage.countData
+//         && JSON.parse(localStorage.countData)) {
+//           // retrieve and parse the JSON
+//           countData = JSON.parse(localStorage.countData);
+//     }
+//   }
+
+//   // If a new version of the app is available, then update the cached version
+//   window.applicationCache.addEventListener('updateready',function(){
+//     window.applicationCache.swapCache();
+//     location.reload();
+//   });
+// });
+
+
 
